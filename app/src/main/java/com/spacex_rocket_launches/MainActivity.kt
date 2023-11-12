8885f42d8f7df5b2dfd058a6ff7a396cec9e1f41
Kekuz <anton.edlenko@yandex.ru>
2023-11-12 18:06:55 +0300

    Init commit

---
+++ app/src/main/java/com/spacex_rocket_launches/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.spacex_rocket_launches
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
